b33315a92f06e149b2a83724967ed1caf20344c6
blackwidow28 <39679875+blackwidow28@users.noreply.github.com>
2021-11-27 01:09:19 +0000

    first commit
    plan for game

---
+++ who_wants_to_be_a_millionaire.py
@@ -0,0 +1,20 @@
+#Who wants to be a millionaire game. 
+"""
+
+Question number	Question value
+1	£100
+2	£200
+3	£300
+4	£500
+5	£1,000 * guranteed cash
+6	£2,000
+7	£4,000
+8	£8,000
+9	£16,000
+10	£32,000 * guranteed cash
+11	£64,000
+12	£125,000
+13	£250,000
+14	£500,000
+15	£1,000,000
+"""
