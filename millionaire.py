0d220987c38657f55e255f5a2e49a2853cc6d886
blackwidow28 <39679875+blackwidow28@users.noreply.github.com>
2021-11-27 18:33:35 +0000

    Completed game final
    Made a few aesthetic changes to file.

--- millionaire.py
+++ millionaire.py
@@ -1,30 +1,13 @@
 #Who wants to be a millionaire game. 
-"""
 
-Question number	Question value
-1	£100
-2	£200
-3	£300
-4	£500
-5	£1,000 * guranteed cash
-6	£2,000
-7	£4,000
-8	£8,000
-9	£16,000
-10	£32,000 * guranteed cash
-11	£64,000
-12	£125,000
-13	£250,000
-14	£500,000
-15	£1,000,000
-"""
+
 # Greeting
 
 print("Hello, what's your name?")
 name = input()
-print("Do you want to be a millionaire " + name + "?")
+print("\nDo you want to be a millionaire " + name + "?")
 answer = input("Y? ")
-print("Then lets play! Good luck!")
+print("\nThen lets play! Good luck!")
 
 
 questions_answers = {"£100": ["Who was elected President of the United States in 2017?", "1. Donald Trump", "2. Barack Obama", "3. George Bush", "4. Abraham Lincoln", "*Donald Trump"],
